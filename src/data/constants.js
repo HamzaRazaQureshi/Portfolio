9db91037de327ed9c5907048d43db2800c7da07e
HamzaRazaQureshi <hamzarq999@gmail.com>
2024-02-08 12:55:06 +0500

    Updated portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,255 @@
+export const Bio = {
+  name: "Hamza Raza Qureshi",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "",
+  github: "https://github.com/HamzaRazaQureshi",
+  resume:
+    "https://drive.google.com/file/d/1Y7VZzN-GWH43RTceJ4Q1-GwK90lnaL-w/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/hamza-raza-qureshi-21109a221/",
+  twitter: "",
+  insta: "",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "C++",
+        image:
+          "",
+      },
+      {
+        name: "Matlab",
+        image:
+          "",
+      },
+      {
+        name: "SQL",
+        image:
+          "",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://drive.google.com/file/d/1V4sB8D_hSQX3h8GXywT8WtW3CUrt8P0o/view?usp=drive_link",
+    role: "Python Developer Intern",
+    company: "Info aidTech",
+    date: "Aug 2023 - July 2023",
+    desc: "Demonstrated adaptability and problem-solving skills in software development.",
+    skills: [
+      "Tkinter", "Python", "Time Management", "Program Creation"
+    ],
+    doc: "https://drive.google.com/file/d/1bU4ZYi2J0kQdDshIjENgKFaHN2lzS-e4/view?usp=sharing",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "",
+    school: "Usman Institute of Technology",
+    date: "Octuber 2020 - June 2024",
+    grade: "3.25 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science at Usman Institute of Technology, Karachi. I have completed 7 semesters and have a CGPA of 3.25.",
+    degree: "Bachelors of Science in Computer Science",
+  },
+  {
+    id: 1,
+    img: "",
+    school: "Adamjee Government Science College",
+    date: "April 2018 - March 2020",
+    grade: "71.4%",
+    desc: "I completed my Intermediate Education at Adamjee Govt. Science College, Karachi.",
+    degree: "Pre-Engineering",
+  },
+  {
+    id: 2,
+    // img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    img: "",
+    school: "St. Paul's English High School",
+    date: "April 2008 - March 2018",
+    grade: "86.8%",
+    desc: "I completed my Matriculation education at St. Paul's English High School, Dankuni.",
+    degree: "Computer Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Information System",
+    date: "April 2022",
+    description:
+      "A simple Information System using React & Nodejs",
+    image:
+      "",
+    tags: [
+      "React",
+      "Nodejs",
+      "JavaScript",
+      "HTML",
+      "CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/HamzaRazaQureshi/Information-System",
+    webapp: "https://github.com/HamzaRazaQureshi/Information-System",
+  },
+  {
+    id: 1,
+    title: "EasyGet",
+    date: "April 2023",
+    description:
+      "EasyGet is a similar website to YOUTUBE demo created using HTML, CSS & Js.",
+    image:
+      "",
+    tags: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/HamzaRazaQureshi/EasyGet",
+    webapp: "https://hamzarazaqureshi.github.io/EasyGet/",
+  },
+  {
+    id: 2,
+    title: "Foodies",
+    date: "August 2022",
+    description:
+      "A demo website created using HTML, CSS Bootstrap.",
+    image:
+      "",
+    tags: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/HamzaRazaQureshi/Foodies-website",
+    webapp: "https://hamzarazaqureshi.github.io/Foodies-website/",
+  },
+  {
+    id: 3,
+    title: "Shoppion",
+    date: "Octuber 2022",
+    description:
+      "Developed SPA Ecommerce Website using HTML, CSS, JavaScript & Bootstrap.",
+    image:
+      "",
+    tags: [
+      "HTML",
+      "CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/HamzaRazaQureshi/Ecommerce-Website",
+    webapp: "https://hamzarazaqureshi.github.io/Ecommerce-Website/",
+  },
+  {
+    id: 4,
+    title: "SocoMedia",
+    date: "September 2022",
+    description:
+      "A social media website using HTML, CSS & Javascript.",
+    image:
+      "",
+    tags: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/HamzaRazaQureshi/SocialMedia-Website",
+    webapp: "https://hamzarazaqureshi.github.io/SocialMedia-Website/?",
+  },
+];
+
+// export const TimeLineData = [
+//   { year: 2017, text: "Started my journey" },
+//   { year: 2018, text: "Worked as a freelance developer" },
+//   { year: 2019, text: "Founded JavaScript Mastery" },
+//   { year: 2020, text: "Shared my projects with the world" },
+//   { year: 2021, text: "Started my own platform" },
+// ];
